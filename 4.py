aba7af51953a24b21d3e32491e17801a4f7b4766
otabek7997 <reyimbayevotabek7@gmail.com>
2025-07-04 16:14:58 +0500

    done

--- 4.py
+++ 4.py
@@ -1,4 +1,12 @@
-orders = [(102, "Ali"), (99, "Vali"), (150, "Sami")]
+from typing import List, Tuple
 
-even_orders = [order for order in orders if order[0] % 2 == 0]
+def filter_orders(orders: List[Tuple]) -> List[Tuple]:
+    result = list()
+    for order in orders:
+        if order[0] % 2 == 0:
+            result.append(order)
+    return result        
+
+orders = [(102, "Ali"), (99, "Vali"), (150, "Sami")]
+even_orders = filter_orders(orders)
 print(even_orders)
