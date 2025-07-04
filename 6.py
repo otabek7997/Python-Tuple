96d574acc80614721be6a865fd5e4d6869aa260e
otabek7997 <reyimbayevotabek7@gmail.com>
2025-07-04 16:21:59 +0500

    done

--- 6.py
+++ 6.py
@@ -1,12 +1,19 @@
+from typing import List, Tuple
+
+def get_domains(emails:Tuple[Tuple]) -> List[str]:
+    domains = list()
+    for email in emails:
+        i = email[1].find('@')
+        domain = email[1][i + 1]
+        domains.append(domain)
+
+    return domains      
+
+
+
+
 emails = (("Ali", "ali@gmail.com"),
 ("Vali", "vali@yandex.ru"),
 ("Sami", "sami@mail.ru"))
-
-domains = []
-
-for item in emails:
-    email = item[1] 
-    domain = email.split("@")[1]   
-    domains.append(domain)
-
-print(domains)
+domain = get_domains(email)
+print(domain)
